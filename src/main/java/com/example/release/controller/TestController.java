0bee1cb6bf3412f5f3e6021a9927d78cfdad1569
Surjit Singh Bhachu <sbhachu@gmail.com>
2021-10-02 21:20:25 +0100

    Fix controlled

--- src/main/java/com/example/release/controller/TestController.java
+++ src/main/java/com/example/release/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
     
     @GetMapping(path = "/")
     public String index() {
-        return "homepage making another change";
+        return "homepage making another change ff gg";
     }
 }
