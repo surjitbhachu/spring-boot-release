23e2541305dc706791c392b85558fb3a37d98354
Surjit Singh Bhachu <sbhachu@gmail.com>
2021-10-02 14:59:19 +0100

    Initial revision

---
+++ src/test/java/com/example/release/ReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.release;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
